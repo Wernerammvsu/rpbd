2b9a6de29a6bc53be55a924bc0420d5e13f8b30b
Екатерина <wernerammvsu@yandex.ru>
2020-10-13 18:25:42 +0300

    Сотворение базы данных
    Signed-off-by: Екатерина <wernerammvsu@yandex.ru>

---
+++ GameOfThrones.py
@@ -0,0 +1,85 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def sql_connection():
+    try:
+        con = sqlite3.connect('GameOfThrones.db')
+        return con
+    except Error:
+        print(Error)
+
+
+def sql_init(con, table):
+    cursorObj = con.cursor()
+    cursorObj.execute('create table if not exists ' + table)
+    con.commit()
+
+
+con = sql_connection()
+con.cursor().execute('PRAGMA foreign_keys = ON')
+con.commit()
+# con.cursor().execute('drop table if exists Character')
+# con.commit()
+# con.cursor().execute('drop table if exists Actor')
+# con.commit()
+
+
+sql_init(con, 'Character(id integer PRIMARY KEY, '
+              'name text, '
+              'surname text, '
+              'dead integer, '
+              'id_partner integer, '
+              'id_mother integer, '
+              'id_father integer, '
+              'age integer, '
+              'image text, '
+              'description text, '
+              'side text)')
+
+sql_init(con, 'Actor(id integer PRIMARY KEY, '
+              'name text, '
+              'surname text, '
+              'image text, '
+              'age integer, '
+              'country text, '
+              'description text)')
+
+sql_init(con, 'House(id integer PRIMARY KEY, '
+              'area text, '
+              'motto text, '
+              'description text)')
+
+sql_init(con, 'Castle(id integer PRIMARY KEY, '
+              'name text, '
+              'description text)')
+
+sql_init(con, 'Character_Actor(id_character integer, '
+              'id_actor integer, '
+              'time integer, '
+              'FOREIGN KEY(id_character) REFERENCES Character(id), '
+              'FOREIGN KEY(id_actor) REFERENCES Actor(id))')
+
+sql_init(con, 'Character_House(id_character integer, '
+              'id_house integer, '
+              'time integer, '
+              'FOREIGN KEY(id_character) REFERENCES Character(id), '
+              'FOREIGN KEY(id_house) REFERENCES House(id) )')
+
+sql_init(con, 'House_Castle(id_house integer, '
+              'id_castle integer, '
+              'time integer, '
+              'FOREIGN KEY(id_house) REFERENCES House(id), '
+              'FOREIGN KEY(id_castle) REFERENCES Castle(id) )')
+
+sql_init(con, 'Vassal(id_owner integer, '
+              'id_vassal integer, '
+              'time integer, '
+              'FOREIGN KEY(id_owner) REFERENCES House(id), '
+              'FOREIGN KEY(id_vassal) REFERENCES House(id))')
+
+sql_init(con, 'House_Head(id_character integer, '
+              'id_house integer, '
+              'time integer, '
+              'FOREIGN KEY(id_character) REFERENCES Character(id), '
+              'FOREIGN KEY(id_house) REFERENCES House(id) )')
---
+++ Insert.py
@@ -0,0 +1,89 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def sql_connection():
+    try:
+        con = sqlite3.connect('GameOfThrones.db')
+        return con
+    except Error:
+        print(Error)
+
+
+def sql_input(con, table,data):
+    try:
+        cursorObj = con.cursor()
+        ID_current = cursorObj.execute('Select max(id) from '+table).fetchone()[0]
+        print(ID_current)
+        if ID_current == None:
+            ID_current = 0
+        else:
+            ID_current += 1
+        cursorObj.execute('Insert into ' + table + ' values(' + str(ID_current) + ',' + data + ')')
+        con.commit()
+        print('sucsses Insert ')
+    except Error as err:
+        print(err)
+
+
+def sql_input_non_id(con, table,data):
+    try:
+        cursorObj = con.cursor()
+        cursorObj.execute('Insert into ' + table + ' values(' + data + ')')
+        con.commit()
+        print('sucsses Insert ')
+    except Error as err:
+        print(err)
+"""
+def convertToBinaryData(filename):
+    #Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        blobData = file.read()
+    return blobData
+
+"""
+con = sql_connection()
+con.cursor().execute('')
+con.commit()
+
+# empPhoto = convertToBinaryData('ДжонСноу.jpg')
+# print(empPhoto)
+"""
+sql_input(con, 'Character', "'Джон', 'Сноу', -1, 1, 2, 3, 22, 'ДжонСноу.jpg', 'Вечно ничего не знает', 'Дейнерис'")
+sql_input(con, 'Character', "'Дейнерис', 'Таргариен', 806020, 1, 54, 55, 22, 'ДейнерисТаргариен.jpg', 'Мать драконов', 'Дейнерис'")
+sql_input(con, 'Character', "'Рейгар', 'Таргариен', 0, 3, 54, 55, 23, 'РейгарТаргариен.jpg', 'Носил красные доспехи', 'Рейгар'")
+sql_input(con, 'Character', "'Лиана', 'Старк', 0, 2, 56, 57, 17, 'ЛианаСтарк.jpg', 'Леди Винтерфелла', 'Рейгар'")
+sql_input(con, 'Character', "'Арья', 'Старк', -1, -1, 60, 61, 17, 'АрьяСтарк.jpg', 'Valar Morgulis', 'Бран Старк'")
+"""
+print(con.cursor().execute('select * from Character').fetchall())
+"""
+sql_input(con, 'Actor', "'Кит', 'Харингтон', 'ДжонСноу.jpg', 33, 'UK', 'Женат на Роуз Лессли' ")
+sql_input(con, 'Actor', "'Эмилия', 'Кларк', 'ДейнерисТаргариен.jpg', 33, 'UK', 'Снималась в терминаторе' ")
+sql_input(con, 'Actor', "'Уилф', 'Сколдинг', 'РейгарТаргариен.jpg', 30, 'UK', 'Малоизвесный актёр' ")
+sql_input(con, 'Actor', "'Эшлинг', 'Франчози', 'ЛианаСтарк.jpg', 27, 'UK', 'Ирландская актриса' ")
+sql_input(con, 'Actor', "'Мэйси', 'Уильямс', 'АрьяСтарк.jpg', 23, 'UK', 'Две номинации на премию Эмми' ")
+"""
+print(con.cursor().execute('select * from Actor').fetchall())
+"""
+sql_input(con, 'House', "'Север', 'Зима близко', 'Старк' ")
+sql_input(con, 'House', "'Запад', 'Услышь мой рёв', 'Ланнистеры' ")
+sql_input(con, 'House', "'Железные  острова', 'Мы не сеем', 'Грейджои' ")
+sql_input(con, 'House', "'Дорн', 'Непреклонные, несгибаемые, несдающиеся', 'Мартеллы' ")
+"""
+print(con.cursor().execute('select * from House').fetchall())
+"""
+sql_input(con, 'Castle', "'Винтерфелл', 'Самый лучший замок Севера' ")
+sql_input(con, 'Castle', "'Красный замок', 'Укрепление в Королевской гавани' ")
+sql_input(con, 'Castle', "'Хайгарден', 'Родовой замок Тиреллов' ")
+sql_input(con, 'Castle', "'Пайк', 'Чертог на Железных островах' ")
+sql_input(con, 'Castle', "'Штормовой предел', 'Непреступная крепость' ")
+"""
+print(con.cursor().execute('select * from Castle').fetchall())
+"""
+sql_input_non_id(con, 'Character_Actor', " 0, 0, 0")
+sql_input_non_id(con, 'Character_Actor', " 1, 1, 0")
+sql_input_non_id(con, 'Character_Actor', " 2, 2, 0")
+sql_input_non_id(con, 'Character_Actor', " 3, 3, 0")
+sql_input_non_id(con, 'Character_Actor', " 4, 4, 0")
+"""
+print(con.cursor().execute('select * from Character_Actor').fetchall())
